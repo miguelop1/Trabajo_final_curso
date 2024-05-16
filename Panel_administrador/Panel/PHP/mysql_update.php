3e57f17fbbbc2cc36b4e25c3d6153d851dcb21fa
MR.Sasma <miguel.suarez.gonzalez.business@gmail.com>
2024-05-16 10:54:53 +0100

    Connection from logging to panel

---
+++ Panel_administrador/Panel/PHP/mysql_update.php
@@ -0,0 +1,20 @@
+<?php
+    include('connection.php'); 
+
+    $stmt = $con->prepare("INSERT INTO admin_config (gmail, text_to_receive, server_name, trigger_for_activation) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $gmail, $textToReceive, $serverName, $triggerForActivation);
+
+    $gmail = $_POST['gmailToReceive'];
+    $textToReceive = $_POST['textToReceive'];
+    $serverName = $_POST['serverName'];
+    $triggerForActivation = $_POST['triggerForActivation'];
+
+    if ($stmt->execute()) {
+        echo "Configuration changed successfully.";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $con->close();
+?>
